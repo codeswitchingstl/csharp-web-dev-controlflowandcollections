21b8ce3e310d43acf1713f158ac364bf270bb71d
codeswitchingstl <codeswitching@icloud.com>
2022-08-07 16:56:03 -0500

    mostly done dictionary exercises with issues re. KeyValuePair

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -6,7 +6,34 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+
+            // Get student names and ID Numbers
+            do
+            {
+                Console.WriteLine("Student: ");
+                newStudent = Console.ReadLine();
+
+                if (newStudent != "")
+                {
+                    Console.WriteLine("ID Number: ");
+                    int idNumber = int.Parse(Console.ReadLine());
+                    students.Add(idNumber, newStudent);
+                }
+
+            } while (!Equals(newStudent, ""));
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Value + "'s ID: " + student.Key);
+            }
+            Console.WriteLine("Number of students in roster: " + students.Count);
         }
     }
 }
