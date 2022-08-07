de24e72d5b0e9d650f744bde536a493deea18479
codeswitchingstl <codeswitching@icloud.com>
2022-08-07 14:05:43 -0500

    forgot to add in modified loop to print odd numbers

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,9 +7,17 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             int[] numberArray = { 1, 1, 2, 3, 5, 8 };
+            //original version of loop
             foreach (int num in numberArray)
             {
                 Console.WriteLine(num);
+            }
+
+            //loop to print only odd numbers
+            foreach (int num in numberArray)
+            {
+                if (num % 2 != 0)
+                    Console.WriteLine(num);
             }
         }
     }
