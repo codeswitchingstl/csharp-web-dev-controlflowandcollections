11e7d612b6a3193ab20c7065db6df4ef57f0a4a6
codeswitchingstl <codeswitching@icloud.com>
2022-08-07 15:26:37 -0500

    finished string exercises

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,11 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string greenEggs = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            string[] words = greenEggs.Split(' ');
+            string[] sentences = greenEggs.Split('.');
+            Console.WriteLine(string.Join("/", words));
+            Console.WriteLine(string.Join("/", sentences));
         }
     }
 }
