74b8ca21e5cf0b6cb2657057cd602cb4b06ff1a8
codeswitchingstl <codeswitching@icloud.com>
2022-08-07 13:53:27 -0500

    array exercises complete

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,11 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] numberArray = { 1, 1, 2, 3, 5, 8 };
+            foreach (int num in numberArray)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
