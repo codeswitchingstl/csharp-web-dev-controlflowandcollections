032ea30800ca58b9a894a87610b503b630da518b
codeswitchingstl <codeswitching@icloud.com>
2022-08-09 14:35:34 -0500

    starting over on some exercises

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace DictionaryExercises
 {
@@ -6,34 +7,7 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> students = new Dictionary<int, string>();
-            string newStudent;
-
-            Console.WriteLine("Enter your students (or ENTER to finish):");
-
-            // Get student names and ID Numbers
-            do
-            {
-                Console.WriteLine("Student: ");
-                newStudent = Console.ReadLine();
-
-                if (newStudent != "")
-                {
-                    Console.WriteLine("ID Number: ");
-                    int idNumber = int.Parse(Console.ReadLine());
-                    students.Add(idNumber, newStudent);
-                }
-
-            } while (!Equals(newStudent, ""));
-
-            // Print class roster
-            Console.WriteLine("\nClass roster:");
-
-            foreach (KeyValuePair<int, string> student in students)
-            {
-                Console.WriteLine(student.Value + "'s ID: " + student.Key);
-            }
-            Console.WriteLine("Number of students in roster: " + students.Count);
+            Console.WriteLine("Hello World!");
         }
     }
 }
--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,7 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+
         }
     }
 }
