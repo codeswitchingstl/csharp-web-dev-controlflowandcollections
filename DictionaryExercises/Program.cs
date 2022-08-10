3243239335f9bef75f5d47096b9aff16981fda11
codeswitchingstl <codeswitching@icloud.com>
2022-08-09 19:04:12 -0500

    updated/completed dictionary exercises

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,13 +1,44 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
 
-namespace DictionaryExercises
+namespace DictionaryGradebook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<double, string> students = new Dictionary<double, string>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+
+            // Get student names and ID numbers
+            do
+            {
+                Console.WriteLine("Student: ");
+                string input = Console.ReadLine();
+                newStudent = input;
+
+                if (!Equals(newStudent, ""))
+                {
+                    Console.WriteLine("ID Number: ");
+                    input = Console.ReadLine();
+                    double idNumber = double.Parse(input);
+                    students.Add(idNumber, newStudent);
+
+                    // Read in the newline before looping back
+                    Console.ReadLine();
+                }
+
+            } while (!Equals(newStudent, ""));
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+
+            foreach (KeyValuePair<double, string> student in students)
+            {
+                Console.WriteLine(student.Value + " (" + student.Key + ")");
+            }
         }
     }
 }
