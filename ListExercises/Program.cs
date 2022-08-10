3421b3b518281ad8d64bd299f4894121b4d583f0
codeswitchingstl <codeswitching@icloud.com>
2022-08-09 20:49:38 -0500

    updated list exercises but needs to be fixed

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -7,7 +7,41 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
+            List <int> nums = new List<int> { 1, 2, 4, 6, 7, 9, 12, 14, 19, 22 };
+            ListStuff.SumEven(nums);
 
+            List<string> wordList = new List<string> { "earth", "right", "faces", "pumpkin", "fuzzy", "pencil", "band", "gum", "moon", "rocket", "puppy", "was" };
+           
+
+        }
+
+        public class ListStuff
+        {
+            public static int SumEven(List<int> nums)
+            {
+                int total = 0;
+                foreach (int integer in nums)
+                {
+                    if (integer % 2 == 0)
+                    {
+                        total += integer;
+                    }
+                }
+                Console.WriteLine(total);
+                return total;
+            }
+
+            public static void printFiveLetterWords(List<string> wordlist)
+            {
+                foreach (string word in wordlist)
+                {
+                    if (word.Length == 5)
+                    {
+                        Console.WriteLine(word);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
